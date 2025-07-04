09a56463f6068edd163b3508f0b18b09bc9914de
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-07-03 21:41:11 -0700

    define the handlers for the inputText and submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,19 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({ todos, setTodos, inputText, setInputText }) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
   return (
     <form>
       <input
