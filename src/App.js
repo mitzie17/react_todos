c76e37bcacec697eb9156783b52476622c4ad90a
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-07-03 21:40:09 -0700

    create state for the input and todos, and passed the variable and functions to the Form componentas well as the inputText variable to the TodoList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
-    <div>
-      <Form />
-      <TodoList />
+    <div className="App">
+      <header>Mitzi's Todo List</header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList inputText={inputText} />
     </div>
   );
 }
