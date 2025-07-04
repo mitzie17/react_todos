9ca3ec282552b2d496e6a936ef6c97670a77d8f4
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-07-03 21:32:55 -0700

    import Form and TodoList components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div>
+      <Form />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
