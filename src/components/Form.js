9ff00119ee6e50a8bc42adec9011325629af2e85
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-07-03 21:33:52 -0700

    create basic form with inputs and a button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form>
+      <input
+        type="text"
+        className="todo-input"
+        onChange={inputTextHandler}
+        value={inputText}
+      />
+      <button type="submit" className="todo-button" onClick={submitTodoHandler}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="incomplete">Incomplete</option>
+        </select>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
