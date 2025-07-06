28829db843a74cf213a1a5d30a8b4273e61fb4fd
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-07-05 22:54:00 -0700

    create state for filtered todos and define filterHandler

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  // State
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  // Functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "incomplete":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>Mitzi's Todo List</header>
@@ -14,6 +31,7 @@ function App() {
         setTodos={setTodos}
         inputText={inputText}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
       <TodoList todos={todos} setTodos={setTodos} inputText={inputText} />
     </div>
